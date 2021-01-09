79044c1858c2a6c8857e9251f6f99cc77b76d83c
Pietro Local <Pietro Local@192.168.1.35>
2021-01-09 16:48:43 +0100

    go

---
+++ src/onlinemarket/readnwrite/RnW.java
@@ -0,0 +1,58 @@
+package onlinemarket.readnwrite;
+
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.TreeSet;
+
+
+public abstract class RnW<T> extends TreeSet<T>{
+	private static final long serialVersionUID = 0L;
+	
+	protected final String filepath;
+	protected abstract void errorReading();
+	
+	public RnW(String filepath) {
+		this.filepath = filepath;
+	}
+	
+	
+	public synchronized boolean read() {
+		clear();
+		
+		try(ObjectInputStream objectIn = new ObjectInputStream(new FileInputStream(filepath))){
+			while(true)
+				add((T) ObjectInputStream.readObject());
+		}catch(EOFException e) {
+			return true;
+		}catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}catch(IOException e) {
+			System.err.println(filepath +": " + ((e instanceof FileNotFoundException)? "file not found!" : "read failed"));
+			errorReading();
+			write();
+			return false;
+		}
+				
+				
+	}
+	
+	
+	public synchronized boolean write() {
+		try(ObjectOutputStream objectOut = new ObjectOutputStream(new FileOutputStream(filepath, false))){
+			for(T obj : this)
+				objectOut.writeObject(obj);
+			
+		}catch(IOException e) {
+			System.err.println(filepath + ": wwrite failed");
+			return false;
+		}
+		return true;
+	}
+	
+
+}
