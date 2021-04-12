845c0b9f106a849aaa66f01ef87ba586ccce7294
Pietro Local <Pietro Local@192.168.1.36>
2021-04-12 17:35:22 +0200

    c

---
+++ src/onlinemarket/stages/FirstGui.java
@@ -0,0 +1,36 @@
+package onlinemarket.stages;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+
+public class FirstGui extends BorderPane{
+	
+	private Label Login, Register;
+	private Button toLogin, createAccount;
+	
+	public FirstGui() {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FirstStage.fxml"));
+		fxmlLoader.setRoot(this);
+		fxmlLoader.setController(this);
+		
+		try {
+			fxmlLoader.load();
+		} catch (IOException exception) {
+			throw new RuntimeException(exception);
+		}
+		
+		
+	}
+	
+
+}
--- src/onlinemarket/stages/FirstStage.java
+++ src/onlinemarket/stages/FirstStage.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -15,9 +15,41 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 
 
+
 public class FirstStage extends Stage{
+	
+	private BorderPane layout;
+	public FirstGui firstGui;
+	
 	public FirstStage() {
-		Main.loadingstage.close();
+		
+		
+			new Thread(() -> {
+				layout = new BorderPane();
+				firstGui = new FirstGui();
+		    	layout.setCenter(firstGui);
+				
+		    	
+				setTitle(Main.title);
+				getIcons().add(Main.logo);
+				setHeight(420);
+				setWidth(800);
+				setResizable(false);
+				
+				setOnCloseRequest(e -> {
+					Platform.exit();
+					System.exit(0);
+				});
+				
+				Platform.runLater(() -> {
+			        setScene(new Scene(layout));
+			        show();		
+			        Main.loadingstage.hide();
+				}); 
+				
+			}).start();
+		
+		
 	}
 	
 	
