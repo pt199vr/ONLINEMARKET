065801922fb40f1c34542eecd6d120affa527618
diber <diber@LAPTOP-07HAUT90>
2021-04-30 12:25:46 +0200

    department e comparatore prodotti

---
+++ src/onlinemarket/departments/Department.java
@@ -0,0 +1,56 @@
+package onlinemarket.departments;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+import onlinemarket.Main;
+import onlinemarket.product.ProdComp;
+public class Department implements Comparable<Department>, Comparator<Department>, Serializable{
+	//Serial ID
+	
+	private String name;
+	public final ProdComp prod;
+	
+	public Department(String name) {
+		this(name,new ProdComp());
+	}
+	
+	public Department(String name, ProdComp prod) {
+		this.name=name;
+		this.prod=prod;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	@Override
+	public int compare(Department o1, Department o2) {
+		// TODO Auto-generated method stub
+		return o1.compareTo(o2);
+	}
+
+	@Override
+	public int compareTo(Department o) {
+		// TODO Auto-generated method stub
+		return name.compareToIgnoreCase(o.name);
+	}
+	@Override
+	public boolean equals(Object other) {
+		return other instanceof Department && ((Department)other).name.equalsIgnoreCase(name);
+	}
+	
+	@Override
+	public int hashCode() {
+		return name.hashCode();
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s : %s",name,prod);
+	}
+}
---
+++ src/onlinemarket/product/ProdComp.java
@@ -0,0 +1,93 @@
+package onlinemarket.product;
+
+import java.util.Comparator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class ProdComp {
+	//Serial ID
+	
+	public static Comparator<Product> getAscPrice(){
+		return new Comparator<Product>(){
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				int diff = p1.getPrice().compareTo(p2.getPrice());
+				
+				if(diff == 0) {
+					diff = p1.getName().compareToIgnoreCase(p2.getName());
+				}
+				if(diff == 0) {
+					diff = p1.getBrand().compareToIgnoreCase(p2.getBrand());
+				}
+				return diff;
+			}
+				
+		};
+	}
+	public static Comparator<Product> getDescPrice(){
+		return new Comparator<Product>(){
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				int diff = p2.getPrice().compareTo(p1.getPrice());
+				
+				if(diff == 0) {
+					diff = p2.getName().compareToIgnoreCase(p1.getName());
+				}
+				if(diff == 0) {
+					diff = p2.getBrand().compareToIgnoreCase(p1.getBrand());
+				}
+				return diff;
+			}
+				
+		};
+	}
+	public static Comparator<Product> getAscBrand(){
+		return new Comparator<Product>(){
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				int diff = p1.getName().compareToIgnoreCase(p2.getName());
+				
+				
+				if(diff == 0) {
+					diff = p1.getBrand().compareToIgnoreCase(p2.getBrand());				
+					}
+				
+				if(diff == 0) {
+					diff = p1.getPrice().compareTo(p2.getPrice());
+				}				
+				return diff;
+			}
+		};
+	}
+	public static Comparator<Product> getDescBrand(){
+		return new Comparator<Product>(){
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				int diff = p2.getName().compareToIgnoreCase(p1.getName());
+				
+				
+				if(diff == 0) {
+					diff = p2.getBrand().compareToIgnoreCase(p1.getBrand());				
+					}
+				
+				if(diff == 0) {
+					diff = p2.getPrice().compareTo(p1.getPrice());
+				}				
+				return diff;
+			}
+		};
+	}
+	//ritornare prodotti ordinati
+	//public SortedSet<Product> get
+		
+	public Double TotalPrice() {
+		Double res = 0D;
+		/*for(Product prod : )
+			res += prod.getPrice();*/
+		return res;
+	}
+}
