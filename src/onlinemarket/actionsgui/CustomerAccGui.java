ccc37f8b3c707da3b54368b83e73447615e16bca
diber <diber@LAPTOP-07HAUT90>
2021-05-14 12:18:07 +0200

    go

--- src/onlinemarket/actionsgui/CustomerAccGui.java
+++ src/onlinemarket/actionsgui/CustomerAccGui.java
@@ -5,13 +5,18 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import onlinemarket.stages.ShopStageGui;
 
 public class CustomerAccGui extends AnchorPane{
+	
 	@FXML
 	private TextField NameT, SurnameT,MailT,CityT,CAPT,CelT;
+	@FXML
+	private PasswordField PasswordT;
 	@FXML
 	private Button DeleteB,BackB;
 	
@@ -27,7 +32,26 @@ public class CustomerAccGui extends AnchorPane{
 			throw new RuntimeException(e);
 		}
 		
+
+		DeleteB.setOnAction(e -> delete());
+		DeleteB.setOnKeyPressed(e->{
+			if(e.getCode()==KeyCode.ENTER)
+				delete();
+		});
 		
+		BackB.setOnAction(e -> mod());
+		BackB.setOnKeyPressed(e->{
+			if(e.getCode() == KeyCode.ENTER)
+				mod();
+		});
+		
+	}
+	@FXML
+	private void delete() {
+		
+	}
+	@FXML
+	private void mod() {
 		
 	}
 
--- src/onlinemarket/actionsgui/EditorAccGui.java
+++ src/onlinemarket/actionsgui/EditorAccGui.java
@@ -4,15 +4,35 @@ import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
+import onlinemarket.Main;
+import onlinemarket.account.Address;
+import onlinemarket.account.EditorAccount;
+import onlinemarket.account.Email;
+import onlinemarket.account.Password;
 import onlinemarket.stages.EditorShopStageGui;
 
 public class EditorAccGui extends AnchorPane{
+	@FXML
+	private TextField NameT,SurnameT,MailT,CelT;
+	@FXML
+	private PasswordField PasswordT;
+	@FXML
+	private Label IDL;
+	@FXML
+	private Button ConfirmB;
+	@FXML
+	private ChoiceBox RoleCB;
 	
 	public EditorAccGui(EditorShopStageGui f) {
-		
-		//ciao
 		
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("EditorAccountView.fxml"));
 		fxmlLoader.setRoot(this);
@@ -24,6 +44,23 @@ public class EditorAccGui extends AnchorPane{
 			throw new RuntimeException(e);
 		}
 		
+		ConfirmB.setOnAction(e -> mod());
+		ConfirmB.setOnKeyPressed(e->{
+			if(e.getCode() == KeyCode.ENTER)
+				mod();
+		});
+		
 	}
-
+	
+	@FXML
+	private void mod() {
+		String name = NameT.getText(), surname = SurnameT.getText();
+		
+		if(name.isBlank() || surname.isBlank()) {
+			Alert a= new Alert(Alert.AlertType.NONE,"Fill all the fields",ButtonType.OK);
+			a.showAndWait();
+			return;
+		}
+	}
 }
+
