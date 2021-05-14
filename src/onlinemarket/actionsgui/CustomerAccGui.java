8fcb8c8f922814f51f49f5c0aa200a05fd0d1c31
diber <diber@LAPTOP-07HAUT90>
2021-05-14 11:08:25 +0200

    go

--- src/onlinemarket/actionsgui/CustomerAccGui.java
+++ src/onlinemarket/actionsgui/CustomerAccGui.java
@@ -2,11 +2,18 @@ package onlinemarket.actionsgui;
 
 import java.io.IOException;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import onlinemarket.stages.ShopStageGui;
 
 public class CustomerAccGui extends AnchorPane{
+	@FXML
+	private TextField NameT, SurnameT,MailT,CityT,CAPT,CelT;
+	@FXML
+	private Button DeleteB,BackB;
 	
 	public CustomerAccGui(ShopStageGui f) {
 		
@@ -19,6 +26,8 @@ public class CustomerAccGui extends AnchorPane{
 		}catch(IOException e) {
 			throw new RuntimeException(e);
 		}
+		
+		
 		
 	}
 
