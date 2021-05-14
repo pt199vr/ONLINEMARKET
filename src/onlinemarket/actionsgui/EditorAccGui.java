f25043178f9280e42e37e0cee231821593559cfd
diber <diber@LAPTOP-07HAUT90>
2021-05-14 11:12:19 +0200

    go

--- src/onlinemarket/actionsgui/EditorAccGui.java
+++ src/onlinemarket/actionsgui/EditorAccGui.java
@@ -2,13 +2,17 @@ package onlinemarket.actionsgui;
 
 import java.io.IOException;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import onlinemarket.stages.EditorShopStageGui;
 
 public class EditorAccGui extends AnchorPane{
 	
 	public EditorAccGui(EditorShopStageGui f) {
+		
+		//ciao
 		
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("EditorAccountView.fxml"));
 		fxmlLoader.setRoot(this);
